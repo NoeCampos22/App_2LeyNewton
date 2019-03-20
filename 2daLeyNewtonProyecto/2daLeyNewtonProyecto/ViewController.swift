883a7b216d3c237c96ee7a1405ae38b0cda5825d
NoeCampos22 <ama-noe@outlook.com>
2019-03-20 11:47:16 -0600

    Agregar Proyecto

---
+++ 2daLeyNewtonProyecto/2daLeyNewtonProyecto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  2daLeyNewtonProyecto
+//
+//  Created by Alumno on 2/27/19.
+//  Copyright © 2019 itesm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+@IBAction func unwindSimulacion (unwindSegue: UIStoryboardSegue) {
+    
+    }
+    @IBAction func unwindPreguntas (unwindSegue: UIStoryboardSegue) {
+        
+    }
+}
+
