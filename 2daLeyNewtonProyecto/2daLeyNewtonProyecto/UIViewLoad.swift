953e51c14031480282fd491766065f96ca4bbe2c
NoeCampos22 <ama-noe@outlook.com>
2019-05-02 02:12:55 -0500

    ViewController y launch screen

---
+++ 2daLeyNewtonProyecto/2daLeyNewtonProyecto/UIViewLoad.swift
@@ -0,0 +1,21 @@
+//
+//  UIViewLoad.swift
+//  2daLeyNewtonProyecto
+//
+//  Created by Angel Trevino on 5/2/19.
+//  Copyright © 2019 itesm. All rights reserved.
+//
+
+import UIKit
+
+class UIViewLoad: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 2daLeyNewtonProyecto/2daLeyNewtonProyecto/ViewLoadScreen.swift
@@ -0,0 +1,20 @@
+//
+//  ViewLoadScreen.swift
+//  2daLeyNewtonProyecto
+//
+//  Created by Angel Trevino on 5/2/19.
+//  Copyright © 2019 itesm. All rights reserved.
+//
+
+import UIKit
+
+class ViewLoadScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.setFondoGradiente(colorUno: Colores.Sky, colorDos: Colores.Purple)
+    }
+    
+
+}
