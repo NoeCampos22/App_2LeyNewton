5a6c14e4880c3a77a1c37175c1c962440c7cea9e
NoeCampos22 <ama-noe@outlook.com>
2019-05-02 11:49:40 -0500

    Casi final

---
+++ 2daLeyNewtonProyecto/2daLeyNewtonProyecto/InformacionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InformacionViewController.swift
+//  2daLeyNewtonProyecto
+//
+//  Created by Alumno on 5/2/19.
+//  Copyright © 2019 itesm. All rights reserved.
+//
+
+import UIKit
+
+class InformacionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
