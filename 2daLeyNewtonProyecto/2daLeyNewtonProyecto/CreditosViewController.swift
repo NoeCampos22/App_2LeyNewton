5ef333532821f808c9ea01d3e770d0160b8d5b50
NoeCampos22 <ama-noe@outlook.com>
2019-05-05 18:26:30 -0500

    Musica

--- 2daLeyNewtonProyecto/2daLeyNewtonProyecto/CreditosViewController.swift
+++ 2daLeyNewtonProyecto/2daLeyNewtonProyecto/CreditosViewController.swift
@@ -13,9 +13,8 @@ class CreditosViewController: UIViewController {
     @IBOutlet weak var lbNombres: UILabel!
     @IBOutlet weak var lbDiseñador: UILabel!
     @IBOutlet weak var lbMusica: UILabel!
+    @IBOutlet weak var lbInfo: UILabel!
     @IBOutlet weak var btRegresar: UIButton!
-    
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,25 +29,16 @@ class CreditosViewController: UIViewController {
         lbDiseñador.esquinasRedondas(radio: 10)
         lbMusica.esquinasRedondas(radio: 10)
         
-        lbNombres.text = "Noé Campos \n ama-noe@hotmail.com        \nMariana Villegas \n mariana2v@hotmail.com  \nElí Moreira \n eli@gmail.com"
+        lbNombres.text = "Noé Campos \n ama-noe@hotmail.com        \nMariana Villegas \n mariana2v@hotmail.com  \nElí Moreira \n coincidir777@hotmail.com"
         
-        lbDiseñador.text = "Stephany Macías \n stephMacias@gmail.com"
+        lbDiseñador.text = "Stephany Macías \n stefhanyreyes7@gmail.com"
         
         lbMusica.text = "https://www.bendsound.com"
+
+        //lbInfo.text = "https://www.fisicalab.com/apartado/principio-fundamental#contenidos"
         
         btRegresar.esquinasRedondas(radio: 10)
         btRegresar.backgroundColor = Colores.ObscureBlue
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
